66c64fc53ede8746f67248e7a28a880c2ad88dcc
Sarah Sid <109920075+SarahSidAhmed@users.noreply.github.com>
2024-06-22 14:16:54 +0200

    Update csv_downloader.py

--- modules/csv_downloader.py
+++ modules/csv_downloader.py
@@ -35,7 +35,7 @@ def error_csv(file, csv_dir, args_y):
             ans = 'y'
             print(bc.OKBLUE + "Automatic download." + bc.ENDC)
         else:
-            ans = y
+            ans = 'y'
 
         if ans.lower() == 'y':
             folder = str(os.path.basename(file)).split('-')[0]
